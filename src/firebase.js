3764ba9357233a0961e1e245355017482fe845e3
Kpatkar <kiran.patkar@estuate.com>
2023-11-01 21:04:45 +0530

    added firebase json file

--- src/firebase.js
+++ src/firebase.js
@@ -10,16 +10,6 @@ const firebaseConfig = {
   appId: '1:458262799626:web:edfe6a784b0bfdb49c86f3',
 };
 
-//real
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCXskCtpuWucNCd6QgWYKAKcQ0Obc_tfx8',
-//   authDomain: 'kpatkar-yt-clone.firebaseapp.com',
-//   projectId: 'kpatkar-yt-clone',
-//   storageBucket: 'kpatkar-yt-clone.appspot.com',
-//   messagingSenderId: '971161519967',
-//   appId: '1:971161519967:web:2ad48e74b2b7a9bf3f58ea',
-// };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
